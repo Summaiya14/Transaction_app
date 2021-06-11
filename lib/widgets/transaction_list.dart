025e5fdb5a1b1b968f5267e26e70121eadbc50bf
summaiya14 <summaiyamahmod@gmail.com>
2021-06-11 23:52:01 +0500

    adding images

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,7 +9,14 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
+      child: transactions.isEmpty ? Column(
+        children: [
+          Text('No transactions added yet!', style: Theme.of(context).textTheme.title,),
+          SizedBox(height: 10,),
+          Container(height: 200, child: Image.asset('assets/images/waiting.png', fit: BoxFit.cover,)),
+        ],
+      )
+      : ListView.builder(
         itemBuilder: (ctx, index) {
           return Card(
                     child: Row(
